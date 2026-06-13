ca24e62a274ec2abe9cfc646f262210cc85d9b15
Alexander Kykc <mad.kykc@gmail.com>
2026-06-14 01:17:07 +0300

    Add nix-ld quirk for nixos

--- desktop-run-debug.sh
+++ desktop-run-debug.sh
@@ -4,5 +4,8 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "Building and running debug app bundle..."
-
-${SCRIPT_DIR}/gradlew :desktopApp:run
+if [ -f /etc/NIXOS ]; then
+    LD_LIBRARY_PATH=${NIX_LD_LIBRARY_PATH} ${SCRIPT_DIR}/gradlew :desktopApp:run
+else
+    ${SCRIPT_DIR}/gradlew :desktopApp:run
+fi
